33723cb8ba3b5f2413c8f44ad04049fde705f368
day <96800776+daywa1kr@users.noreply.github.com>
2022-07-04 12:34:44 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+import twitter4j.*;
+import java.util.List;
+import java.util.Timer;
+
+public class Main {
+    static Status mTweet;
+    static Maze map=new Maze(5);
+    static Twitter t=new TwitterFactory().getInstance();
+
+    /**
+     * @return the last tweet (the updated maze)
+     */
+    public static Status getLatestTweet() {
+        List<Status> list = null;
+        try {
+            list = t.getUserTimeline("@daywa1kr");
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        return list.get(0);
+    }
+
+    public static void main(String[] args){
+       Timer timer=new Timer();
+       timer.scheduleAtFixedRate(new Update(), 0, 30000); //updates every 15 minutes
+    }
+}
